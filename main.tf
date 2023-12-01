7e2a8cbc133ce3b6f455fe94133ea5013d118700
Abdelrhman <balloabdelrhman@gmai.com>
2023-12-01 08:08:37 +0200

    update modules

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-module "my_module" {
+module "network_module" {
   
-  source = "E:\\ITI\\terraformPractice\\terra-pro\\instances"
+  source = "E:\\ITI\\terraformPractice\\terra-pro\\network"
   
-  vpc_name = var.vpc_name
-  
+    // network input
+    vpc_name = var.vpc_name
+    vpc_ip = var.vpc_ip
+    AZ = var.AZ
+    public_subnets_cidr = var.public_subnets_cidr
+    private_subnets_cidr = var.private_subnets_cidr
+    public_subnet_tags = var.public_subnet_tags
+    private_subnet_tags = var.private_subnet_tags
+}
+
+module "instance_module" {
+    
   
 }
+
